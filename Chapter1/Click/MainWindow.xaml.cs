537c4aa71fd2ad0254dfd952f2325f0099e73c57
Nikita <40354217+Ghoost17@users.noreply.github.com>
2020-03-24 22:30:12 +0500

    CH_1

---
+++ Chapter1/Click/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Click
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       public MainWindow()
+        {
+            InitializeComponent();
+        }
+     protected override void OnKeyDown(KeyEventArgs args)
+        {
+            base.OnKeyDown(args);
+            if (args.Key == Key.Up)
+            {
+                Left -= 0.05 * Width;
+                Top -= 0.05 * Height;
+                Width *= 1.1;
+                Height *= 1.1;
+            }
+            else 
+            if (args.Key == Key.Down)
+            {
+                Left += 0.05 * (Width /= 1.1);
+                Top += 0.05 * (Height /= 1.1);
+            }
+        } }
+   }
+
