51c037c879b7f675e7ae0c72016fb5b6235bbb6b
Nikita <40354217+Ghoost17@users.noreply.github.com>
2020-03-30 00:14:17 +0500

    CH_2,CH_3

---
+++ Chapter2/Chapter_2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Rainbow();
+            Title = "Circle the rainbow";
+        }
+        public void Rainbow()
+        {
+            RadialGradientBrush brush = new RadialGradientBrush();
+            Background = brush;
+
+            brush.GradientStops.Add(new GradientStop(Colors.Red, 0));
+            brush.GradientStops.Add(new GradientStop(Colors.Orange, .17));
+            brush.GradientStops.Add(new GradientStop(Colors.Yellow, .33));
+            brush.GradientStops.Add(new GradientStop(Colors.Green, .5));
+            brush.GradientStops.Add(new GradientStop(Colors.Blue, .67));
+            brush.GradientStops.Add(new GradientStop(Colors.Indigo, .84));
+            brush.GradientStops.Add(new GradientStop(Colors.Violet, 1));
+
+        }
+        
+    }
+}
---
+++ Chapter3/Chapter3/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            ToggleBoldAndItalic();
+            
+        }
+         void RunOnMouseDown(object sender, MouseButtonEventArgs args)
+            {
+                Run run = sender as Run;
+
+                if (args.ChangedButton == MouseButton.Left)
+                    run.FontStyle = run.FontStyle == FontStyles.Italic ?
+                        FontStyles.Normal : FontStyles.Italic;
+
+                if (args.ChangedButton == MouseButton.Right)
+                    run.FontWeight = run.FontWeight == FontWeights.Bold ?
+                        FontWeights.Normal : FontWeights.Bold;
+
+            }
+        public void ToggleBoldAndItalic()
+        {
+            Title = " Toggle and bold & Italic";
+            TextBlock text = new TextBlock();
+            text.FontSize = 32;
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            text.VerticalAlignment = VerticalAlignment.Center;
+            Content = text;
+
+            string strQ = "Yes or no? I don't know.";
+            string[] strW = strQ.Split();
+
+            foreach(string str in strW)
+            {
+                Run run = new Run(str);
+                run.MouseDown += RunOnMouseDown;
+                text.Inlines.Add(run);
+                text.Inlines.Add(" ");
+            }
+
+
+        }
+        
+    }
+}
